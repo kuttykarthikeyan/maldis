9735e26cd6ff5529e0f3ec10573099c883772089
Your name <you@example.com>
2023-04-26 22:13:23 +0530

    a pretty good game

---
+++ maldis.py
@@ -0,0 +1,7 @@
+
+adj=input("Adjectine bro👉")
+verb1=input("A verb please👉")
+verb2=input("A  another verb please👉")
+madlibs=f" hi Computer programmming is so {adj} It makes me so exited  all the time because\
+I love to {verb1}.I stay hydrated amd{verb2}"
+print(madlibs)
